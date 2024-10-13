7813a41e831631a285a2ab9bacb7df8df789fb95
John Walsh <20245424@studentmail.ul.ie>
2024-10-13 17:38:05 +0100

    Added Basic Homepage, OAuth Google Login
    Back end start with File structure, Google OAuth, Basic Homepage

---
+++ src/main/java/com/example/onlypans/onlypans/OnlyPansApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlypans.onlypans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyPansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyPansApplication.class, args);
+	}
+
+}
