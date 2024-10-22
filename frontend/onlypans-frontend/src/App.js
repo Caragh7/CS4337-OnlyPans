80579daf1b3e61981a4a55755223c175af5906f0
MartyD1 <coolkidmarty@gmail.com>
2024-10-22 02:06:00 +0100

    Post component

--- frontend/onlypans-frontend/src/App.js
+++ frontend/onlypans-frontend/src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from "./components/appbar";
+import PostCard from "./components/post";
 
 function App() {
   return (
-    <div>
-        <ResponsiveAppBar/>
-    </div>
+      <div>
+          <ResponsiveAppBar/>
+
+          <div style={styles.scrollableContainer}>
+              <PostCard/>
+              <PostCard/>
+              <PostCard/>
+              <PostCard/>
+              <PostCard/>
+              <PostCard/>
+          </div>
+      </div>
   );
 }
+
+const styles = {
+    scrollableContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        padding: '20px',
+        height: '100%',
+        overflowY: 'scroll',
+        scrollbarWidth: 'thin',
+        gap: '20px',
+    },
+};
 
 export default App;
