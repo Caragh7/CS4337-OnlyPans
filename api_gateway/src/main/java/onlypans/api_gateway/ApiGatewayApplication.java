294194af7d661a4758a8d9d9d9487525eec34a61
Sam Ennis <samennis127@gmail.com>
2024-11-25 22:38:55 +0000

    Fix auth, use web flux security & appropriate security config to support Consul health checks

--- api_gateway/src/main/java/onlypans/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/onlypans/api_gateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package onlypans.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
+@SpringBootApplication()
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
